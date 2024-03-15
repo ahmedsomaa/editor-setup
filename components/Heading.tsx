e23c56c21d2e5408df803687567992e0b501905b
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-15 21:59:18 +0200

    add author, description, canonical, open graph & twitter meta information

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -3,13 +3,46 @@ import Head from "next/head";
 export default function Heading() {
   return (
     <Head>
-      <title>EditorSetup</title>
-      <link rel="icon" href="/favicon.ico" />
+      {/* Meta */}
+      <meta charSet="UTF-8" />
+      <meta name="author" content="Ahmed Abu Qahf" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta
         name="description"
         content="Explore multiple font and theme pairings for Visual Studio Code that
             just work!"
       />
+      <meta name="keywords" content="VS Code, Theme, Font" />
+      <meta name="robots" content="index, follow" />
+
+      {/* Links */}
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="canonical" href="https://editorsetup.vercel.app" />
+
+      {/* Open Graph */}
+      <meta property="og:title" content="EditorSetup" />
+      <meta
+        property="og:description"
+        content="Explore multiple font and theme pairings for Visual Studio Code that
+        just work!"
+      />
+      <meta property="og:url" content="https://editorsetup.vercel.app" />
+      <meta property="og:image" content="/img/og-image.png" />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="https://editorsetup.vercel.app" />
+      <meta name="twitter:creator" content="Ahmed Abu Qahf" />
+      <meta name="twitter:title" content="EditorSetup" />
+      <meta
+        name="twitter:description"
+        content="Explore multiple font and theme pairings for Visual Studio Code that
+        just work!"
+      />
+      <meta name="twitter:image" content="/img/og-image.png" />
+
+      {/* Title */}
+      <title>EditorSetup</title>
     </Head>
   );
 }
