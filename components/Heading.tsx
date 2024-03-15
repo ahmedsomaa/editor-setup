5c6486fc512f7cb675ddf7c995a8079d410e8eb3
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-03-15 17:05:45 +0200

    add app head tag

---
+++ components/Heading.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Heading() {
+  return (
+    <Head>
+      <title>EditorSetup</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta
+        name="description"
+        content="Explore multiple font and theme pairings for Visual Studio Code that
+            just work!"
+      />
+    </Head>
+  );
+}
