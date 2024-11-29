5f68bf664dc0db7c8fefcd4c19ba03865e87c0d2
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-11-29 23:40:37 +0200

    chore: revert back from supabase

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: "lcfuwhtepnjxquhceqnh.supabase.co",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
